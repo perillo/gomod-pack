a2246ca993d31c4417954c5e0fc7eaf2f4e51535
Manlio Perillo <manlio.perillo@gmail.com>
2020-01-28 23:18:01 +0100

    Add the command documentation
    Add the package documentation for the gomod-pack command.

--- main.go
+++ main.go
@@ -2,6 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Command gomod-pack will pre-fill the Go module cache with the current module
+// at the specified version (default to master) and will print the actual
+// versioned module path so that it can be used in a go.mod require directive.
+//
+// Internally, gomod-pack invokes go mod download with a custom environment,
+// where git is configured to resolve the remote module path to the local
+// module root directory and cmd/go is configured to use a direct connection
+// with the checksum database disabled.
 package main
 
 import (
